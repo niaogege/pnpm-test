ff5f6db85658ad2b93a97f05f177539f1300bd25
chendapeng <pengpeng.chen@ximalaya.com>
2022-07-14 19:36:33 +0800

    feat: add set

---
+++ .changeset/chilled-nails-laugh.md
@@ -0,0 +1,6 @@
+---
+"auto-ubt": major
+"player": major
+---
+
+log readme
---
+++ packages/player/src/index.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor() {
+    this.version = "1.0.0";
+  }
+}
+
+export default {
+  player: new Player(),
+};
